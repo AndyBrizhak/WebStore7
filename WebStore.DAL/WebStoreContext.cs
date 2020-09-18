7e22aa478fa7deb3b55910588a3890812bf42664
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-19 07:47:31 +0800

    Add project files.

---
+++ WebStore.DAL/WebStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL
+{
+    public class WebStoreContext : IdentityDbContext<User>
+    {
+        public WebStoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
